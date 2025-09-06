2c6b40a742179e0e11927f6e2ad30c4896dc0714
ahmed <ahmed@AHMED_SAMY>
2025-09-06 20:30:21 +0300

    2

--- S1/Program.cs
+++ S1/Program.cs
@@ -1,11 +1,21 @@
 ﻿
+using S1.Data
 namespace S1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region LINQ - Restriction Operators
+
+            #region 1. Find all products that are out of stock.
+
+            var Result = Pro
+
+
+            #endregion
+
+            #endregion
         }
     }
 }
